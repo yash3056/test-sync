4cff52ff330d4aa7c6b3178d62f9f12e38324b78
yash <73551736+yashlearnpython@users.noreply.github.com>
2021-05-30 11:19:26 +0530

    Create sync_rom.sh

---
+++ sync_rom.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -exv
+
+#sync rom
+repo init --depth=1 -u https://github.com/Wave-Project/manifest -b r -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/yashlearnpython/local_manifest.git --depth=1 -b wave-os .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
