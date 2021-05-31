2a4fd321d72fdc3eec921a0d801bf66a7302e4de
yash <73551736+yashlearnpython@users.noreply.github.com>
2021-05-31 12:26:30 +0530

    try lineage-FE

--- sync_rom.sh
+++ sync_rom.sh
@@ -3,6 +3,6 @@
 set -exv
 
 #sync rom
-repo init --depth=1 -u https://github.com/Wave-Project/manifest -b r -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/yashlearnpython/local_manifest.git --depth=1 -b wave-os .repo/local_manifests
+repo init --depth=1 -u git://github.com/Lineage-FE/manifest.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/yashlearnpython/local_manifest.git --depth=1 -b Lineage-FE .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
