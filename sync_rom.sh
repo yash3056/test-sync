79cb8aaf42d5506fe5216a531aef46223d29cf2d
yash <73551736+yashlearnpython@users.noreply.github.com>
2021-06-20 00:19:07 +0530

    sync aospa

--- sync_rom.sh
+++ sync_rom.sh
@@ -3,6 +3,6 @@
 set -exv
 
 #sync rom
-repo init --depth=1 -u https://github.com/P-404/platform_manifest -b rippa -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/yashlearnpython/local_manifest.git --depth=1 -b p404 .repo/local_manifests
+repo init --depth=1 -u https://github.com/AOSPA/manifest -b ruby -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/yashlearnpython/local_manifest.git -b pa .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
