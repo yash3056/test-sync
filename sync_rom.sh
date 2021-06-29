b866e245be75066b5089af61ec0197aa17a6bd83
yash <73551736+yashlearnpython@users.noreply.github.com>
2021-06-29 08:47:55 +0530

    lets do pe

--- sync_rom.sh
+++ sync_rom.sh
@@ -3,6 +3,6 @@
 set -exv
 
 #sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/yashlearnpython/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/yashlearnpython/local_manifest.git -b pe .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
